c071e28ec8da51c44847b365d440137044addbb4
Ryan Charette <100053398+ryan-charette@users.noreply.github.com>
2022-10-29 11:35:28 -0500

    Create language_vectors.py

---
+++ language_vectors.py
@@ -0,0 +1,10 @@
+# Part 1: Some basics
+import numpy as np
+import numpy.linalg as la
+
+# What is a vector and how can it be represented in python?
+u = np.array([1,2,3])
+
+print("We are working on u: \n", u)
+print("The shape of u is: ", u.shape)
+print("The len of u is: ", len(u))
