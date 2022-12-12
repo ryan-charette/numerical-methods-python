25b6fa48031aa30f040c53534e236c2d8ff6fddb
Ryan Charette <100053398+ryan-charette@users.noreply.github.com>
2022-12-11 18:26:05 -0600

    Add files via upload

---
+++ PDEs.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+def heat_equation():
+
+  D = 0.5 # diffusion coefficient
+  x = np.linspace(0, 1, 100)
+  t = np.linspace(0, 1, 100)
+  dx = x[1]-x[0]
+  dt = t[1]-t[0]
+  IC = np.sin(2*np.pi*(x)) # initial conditions
+  BC = [0,0] # Dirichlet boundary conditions
+
+  u = np.zeros((len(x), len(t)))
+  # enforcing boundary and initial conditions
+  u[0:] = BC[0]
+  u[-1:] = BC[1]
+  u[:,0] = IC
+
+  # define parameter r as
+  r = D*dt/dx**2
+
+  # finite difference scheme
+  A = np.diag([1+2*r]*(len(x)-2),0) + np.diag([-r]*(len(x)-3),-1) + np.diag([-r]*(len(x)-3),1)
+  print(t)
+  print(A)
+  for n in range(1,len(t)):
+    b = u[1:-1,n-1].copy()
+    b[0] = b[0] + r*u[0,n]
+    b[-1] = b[-1] + r*u[-1,n]
+    u[1:-1,n] = np.linalg.solve(A,b)
+    print(u[1:-1,n])
+
+  # animation
+
+  fig, ax = plt.subplots()
+  line, = ax.plot(x, IC)
+
+  def animate(i):
+    line.set_ydata(u[:,i])
+    return line,
+
+  ani = animation.FuncAnimation(
+    fig, animate, interval=50, blit=True, save_count=50)
+  plt.show()
+
+heat_equation()
